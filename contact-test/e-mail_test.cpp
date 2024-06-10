fd366fbdbdab29e86a64671d41efcec7d25ae1e3
Mauricio Gomes <mgpensar@gmail.com>
2024-06-10 17:08:30 -0300

    First commit.

---
+++ contact-test/e-mail_test.cpp
@@ -0,0 +1,23 @@
+// author : Mauricio Gomes
+// license: MIT (https://opensource.org/licenses/MIT)
+
+#include "../../unit-test/test/test.hpp"
+#include "../e-mail.hpp"
+
+
+namespace pensar_digital
+{
+    using namespace pensar_digital::unit_test;
+    namespace cpplib
+    {
+        namespace contact
+        {
+            TEST(Email, true)
+                CHECK(is_valid_email_address(W("x@x.com")), W("0"));
+                Email email(W("x@x.com"));
+                CHECK_EQ(S, email.str (), W("x@x.com"), W("1"));
+
+            TEST_END(Email)
+        } // namespace contact
+    } // namespace cpplib
+} // namespace pensar_digital
